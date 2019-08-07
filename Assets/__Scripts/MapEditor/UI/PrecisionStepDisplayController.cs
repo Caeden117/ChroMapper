4e1f92af693624689e85ee7ccaf2a9af672a605c
Caeden Statia <caeden.s@outlook.com>
2019-08-07 16:53:43 -0700

    Added manual precision step w/textbox

--- Assets/__Scripts/MapEditor/UI/PrecisionStepDisplayController.cs
+++ Assets/__Scripts/MapEditor/UI/PrecisionStepDisplayController.cs
@@ -3,14 +3,24 @@ using UnityEngine;
 
 public class PrecisionStepDisplayController : MonoBehaviour {
 
-    [SerializeField]
-    private AudioTimeSyncController atsc;
-
-    [SerializeField]
-    private TextMeshProUGUI display;
+    [SerializeField] private AudioTimeSyncController atsc;
+    [SerializeField] private TMP_InputField display;
+    private int previousGridMeasureSnapping = 1;
 
 	// Update is called once per frame
 	void Update () {
-        display.text = atsc.gridMeasureSnapping.ToString();
+        if (atsc.gridMeasureSnapping == previousGridMeasureSnapping) return;
+        previousGridMeasureSnapping = atsc.gridMeasureSnapping;
+        display.text = previousGridMeasureSnapping.ToString();
 	}
+
+    public void UpdateManualPrecisionStep(string result)
+    {
+        int newGridMeasureSnapping = 1;
+        if (int.TryParse(result, out newGridMeasureSnapping))
+        {
+            previousGridMeasureSnapping = newGridMeasureSnapping;
+            atsc.gridMeasureSnapping = newGridMeasureSnapping;
+        }
+    }
 }
