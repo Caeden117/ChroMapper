39d90ead52e86631d46a5b0edfca89dbacc10b85
Caeden Statia <caeden.s@outlook.com>
2019-08-09 21:07:44 -0700

    Added Notes Per Second to map stats

--- Assets/__Scripts/MapEditor/UI/CountersPlusController.cs
+++ Assets/__Scripts/MapEditor/UI/CountersPlusController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class CountersPlusController : MonoBehaviour {
 
@@ -9,7 +10,10 @@ public class CountersPlusController : MonoBehaviour {
     [SerializeField] private ObstaclesContainer obstacles;
     [SerializeField] private EventsContainer events;
     [SerializeField] private BPMChangesContainer bpm;
+    [SerializeField] private AudioSource cameraAudioSource;
+    [SerializeField] private AudioTimeSyncController atsc;
     [SerializeField] private TextMeshProUGUI notesMesh;
+    [SerializeField] private TextMeshProUGUI notesPSMesh;
     [SerializeField] private TextMeshProUGUI obstaclesMesh;
     [SerializeField] private TextMeshProUGUI eventsMesh;
     [SerializeField] private TextMeshProUGUI bpmMesh;
@@ -23,16 +27,28 @@ public class CountersPlusController : MonoBehaviour {
         while (true)
         {
             yield return new WaitForSeconds(1); //I wouldn't want to update this every single frame.
-            notesMesh.text = string.Format("Notes: {0}", notes.loadedNotes.Count);
-            obstaclesMesh.text = string.Format("Obstacles: {0}", obstacles.loadedObstacles.Count);
-            eventsMesh.text = string.Format("Events: {0}", events.loadedEvents.Count);
-            bpmMesh.text = string.Format("BPM Changes: {0}", bpm.loadedBPMChanges.Count);
+            if (SelectionController.HasSelectedObjects()) {
+                List<BeatmapObjectContainer> sel = SelectionController.SelectedObjects.OrderBy(x => x.objectData._time).ToList();
+                int notes = SelectionController.SelectedObjects.Where(x => x is BeatmapNoteContainer).Count();
+                notesMesh.text = $"Selected Notes: {notes}";
+                float beatTimeDiff = sel.Last().objectData._time - sel.First().objectData._time;
+                float secDiff = atsc.GetSecondsFromBeat(beatTimeDiff);
+                notesPSMesh.text = $"Selected NPS: {(notes / secDiff).ToString("F2")}";
+            }
+            else {
+                notesMesh.text = $"Notes: {notes.loadedNotes.Count}";
+                notesPSMesh.text = $"Notes Per Second: {(notes.loadedNotes.Count / cameraAudioSource.clip.length).ToString("F2")}";
+            }
+            obstaclesMesh.text = $"Obstacles: {obstacles.loadedObstacles.Count}";
+            eventsMesh.text = $"Events: {events.loadedEvents.Count}";
+            bpmMesh.text = $"BPM Changes: {bpm.loadedBPMChanges.Count}";
         }
 	}
 
     public void ToggleCounters(bool enabled)
     {
         notesMesh.gameObject.SetActive(enabled);
+        notesPSMesh.gameObject.SetActive(enabled);
         obstaclesMesh.gameObject.SetActive(enabled);
         eventsMesh.gameObject.SetActive(enabled);
         bpmMesh.gameObject.SetActive(enabled);
