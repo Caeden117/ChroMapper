090d96c77dfb6bfeec340d7178deb2729363f426
Caeden Statia <caeden.s@outlook.com>
2019-08-26 19:14:40 -0700

    Cleaned up repository

--- Library/Collab/Base/Assets/__Scripts/Map/BeatmapObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BeatmapObject {
-    public abstract float _time { get; }
-    public abstract Type beatmapType { get; }
-    public enum Type { Note, Event, Obstacle, Bomb, Custom }
-}
--- Library/Collab/Original/Assets/__Scripts/Map/BeatmapObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BeatmapObject {
-    public abstract float _time { get; set; }
-    public abstract Type beatmapType { get; set; }
-    public enum Type { Note, Event, Obstacle, Bomb, Custom }
-}
