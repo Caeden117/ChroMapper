dbb70b00ec657337f6932d2877ee09d6926b623b
Caeden Statia <caeden.s@outlook.com>
2019-09-01 22:16:41 -0700

    Upgraded post processing (and controller)

--- Assets/__Scripts/MapEditor/UI/PostProcessingController.cs
+++ Assets/__Scripts/MapEditor/UI/PostProcessingController.cs
@@ -1,27 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
-using UnityEngine.UI;
 
 public class PostProcessingController : MonoBehaviour {
 
     public PostProcessVolume PostProcess;
-
-    public Toggle PostProcessToggle;
-    public Toggle ExtraPostProcessToggle;
-
-    private void Start()
-    {
-        PostProcessToggle.onValueChanged.AddListener((x) => TogglePostProcess(x));
-        ExtraPostProcessToggle.onValueChanged.AddListener((x) => ToggleExtraPostProcess(x));
-    }
-
-    private void OnDestroy()
-    {
-        PostProcessToggle.onValueChanged.RemoveAllListeners();
-        ExtraPostProcessToggle.onValueChanged.RemoveAllListeners();
-    }
 
     public void TogglePostProcess(bool state)
     {
@@ -30,19 +12,8 @@ public class PostProcessingController : MonoBehaviour {
 
     public void ToggleExtraPostProcess(bool extraPostProcess)
     {
-        switch (extraPostProcess)
-        {
-            case true:
-                PostProcess.profile.GetSetting<ChromaticAberration>().intensity.value = 0.5f;
-                PostProcess.profile.GetSetting<Bloom>().softKnee.value = 0.5f;
-                PostProcess.profile.GetSetting<Bloom>().dirtIntensity.value = 15;
-                break;
-            case false:
-                PostProcess.profile.GetSetting<ChromaticAberration>().intensity.value = 0.1f;
-                PostProcess.profile.GetSetting<Bloom>().softKnee.value = 0.25f;
-                PostProcess.profile.GetSetting<Bloom>().dirtIntensity.value = 0;
-                break;
-        }
+        if (extraPostProcess) PostProcess.profile.GetSetting<Bloom>().intensity.value = 5;
+        else PostProcess.profile.GetSetting<Bloom>().intensity.value = 1;
     }
 
 }
