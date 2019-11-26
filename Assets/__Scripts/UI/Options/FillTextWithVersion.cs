3aa57c0757147860198dfabaca323a8df2bce2ca
Caeden Statia <caeden.s@outlook.com>
2019-11-25 19:35:20 -0800

    fill version text

---
+++ Assets/__Scripts/UI/Options/FillTextWithVersion.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class FillTextWithVersion : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<TextMeshProUGUI>().text = $"Version {Application.version}";        
+    }
+}
