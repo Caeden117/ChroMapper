06827a536f0d3b4ae04785b7b247c7755533df31
Caeden Statia <caeden.s@outlook.com>
2019-12-28 23:36:46 -0800

    redid visuals controller

--- Assets/__Scripts/MapEditor/VisualsController.cs
+++ Assets/__Scripts/MapEditor/VisualsController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class VisualsController : MonoBehaviour {
 
-    [SerializeField] GameObject[] togglableGameObjects;
+    [SerializeField] GameObject[] gameObjectsWithRenderersToToggle;
+
+    private List<Renderer> renderers = new List<Renderer>();
+
+    private void Start()
+    {
+        foreach (GameObject go in gameObjectsWithRenderersToToggle)
+            renderers.AddRange(go.GetComponentsInChildren<Renderer>());
+    }
 
     void Update() {
         if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.G))
-            foreach (GameObject go in togglableGameObjects) go.SetActive(!go.activeSelf);
+            foreach (Renderer renderer in renderers) renderer.enabled = !renderer.enabled;
     }
 
 }
