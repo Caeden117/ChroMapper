b17a8836b2266fb9b7f1e3264bc0fc63bfbcf1d7
Ryan Ernest <ryan@rivergroup.com>
2020-01-01 23:26:29 -0500

    Added Metronome to UI

--- Assets/__Scripts/MapEditor/Audio/MetronomeHandler.cs
+++ Assets/__Scripts/MapEditor/Audio/MetronomeHandler.cs
@@ -12,12 +12,28 @@ public class MetronomeHandler : MonoBehaviour
 
     [SerializeField] private AudioUtil audioUtil;
     
+    [SerializeField] private GameObject metronomeUI;
+    private RectTransform metronomeUITransform;
+
+    private void Start()
+    {
+        metronomeUITransform = metronomeUI.GetComponent<RectTransform>();
+    }
+
+    private void LateUpdate()
+    {
+        metronomeUI.SetActive(Settings.Instance.MetronomeVolume != 0f);
+    }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("MeasureLine") && atsc.IsPlaying)
         {
             //Get Measure Name: other.GetComponent<TextMeshProUGUI>().text
             audioUtil.PlayOneShotSound(metronomeSound, Settings.Instance.MetronomeVolume);
+            Vector3 vec = metronomeUITransform.localScale;
+            vec.x = -vec.x;
+            metronomeUITransform.localScale = vec;
         }
     }
 }
