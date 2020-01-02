7108b7006ed6e326e5b811ef57f1139600e871d6
Ryan Ernest <ryan@rivergroup.com>
2020-01-01 22:29:51 -0500

    Added Metronome

---
+++ Assets/__Scripts/MapEditor/Audio/MetronomeHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MetronomeHandler : MonoBehaviour
+{
+    [SerializeField] private AudioTimeSyncController atsc;
+    [SerializeField] private AudioClip metronomeSound;
+
+    [SerializeField] private AudioUtil audioUtil;
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("MeasureLine") && atsc.IsPlaying)
+        {
+            //Get Measure Name: other.GetComponent<TextMeshProUGUI>().text
+            audioUtil.PlayOneShotSound(metronomeSound, Settings.Instance.MetronomeVolume);
+        }
+    }
+}
