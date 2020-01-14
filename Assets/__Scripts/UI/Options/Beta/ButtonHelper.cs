7b6d6d0cf896fb87633b61b9633fea63eac5e848
Ryan <ryan@rivergroup.com>
2020-01-14 11:32:24 -0500

    Got discord Popout animation to work

--- Assets/__Scripts/UI/Options/Beta/ButtonHelper.cs
+++ Assets/__Scripts/UI/Options/Beta/ButtonHelper.cs
@@ -16,16 +16,19 @@ public class ButtonHelper : UIBehaviour, IPointerExitHandler, IPointerEnterHandl
 
     [SerializeField] public TextMeshProUGUI textMeshTabName;
     [SerializeField] public RectTransform discordPopout;
+    [SerializeField] public CanvasGroup discordPopoutCanvas;
     [SerializeField] public Image icon;
     
     private readonly Color _iconColorHover = new Color(0, 0.5f, 1, 1);
     private readonly Color _iconColorSelected = new Color(.78f, 0.47f, 0, 1);
 
+    private Coroutine _discordPopoutCoroutine;
+    
     private void Start()
     {
         _tabManager = transform.parent.parent.GetComponent<TabManager>();//Help
-
         tabId = _tabManager.GetTabId(this);
+        _discordPopoutCoroutine = null;
     }
     
     public void ChangeTab()
@@ -37,40 +40,33 @@ public class ButtonHelper : UIBehaviour, IPointerExitHandler, IPointerEnterHandl
     {
         if(!selected) icon.color = Color.white;
         hovering = false;
-        StopCoroutine(SlideText());
-        StartCoroutine(SlideText());
+        _discordPopoutCoroutine = StartCoroutine(SlideText());
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         if(!selected) icon.color = _iconColorHover;
         hovering = true;
-        StopCoroutine(SlideText());
-        StartCoroutine(SlideText());
+        _discordPopoutCoroutine = StartCoroutine(SlideText());
     }
 
     private IEnumerator SlideText()
     {
+        if(_discordPopoutCoroutine != null) StopCoroutine(_discordPopoutCoroutine);
         
-        
-        //This does not work. It needs to be remade, I'm tired.
-        /*float startTime = Time.time;
-
+        float startTime = Time.time;
         Vector3 zero = new Vector3(0, 1, 1);
         Vector3 one = new Vector3(1, 1, 1);
-
-        float length;
-        if(hovering) length = Vector3.Distance(zero, one);
-        else length = Vector3.Distance(one, zero);
+        
         while (true)
         {
-            discordPopout.localScale = Vector3.Lerp(discordPopout.localScale, hovering ? one : zero, (Time.time - startTime) * 0.0001f / length);
-            if (discordPopout.localScale == one || discordPopout.localScale == zero)
-            {
-                yield return this;
-            }
-        }*/
-        yield return this;
+            Vector3 localScale = discordPopout.localScale;
+            localScale = Vector3.Lerp(localScale, hovering ? one : zero, (Time.time / startTime) * .4f);
+            discordPopout.localScale = localScale;
+            discordPopoutCanvas.alpha = localScale.x;
+            if (discordPopout.localScale == one || discordPopout.localScale == zero) break;
+            yield return new WaitForFixedUpdate();
+        }
     }
     
     private void LateUpdate()
