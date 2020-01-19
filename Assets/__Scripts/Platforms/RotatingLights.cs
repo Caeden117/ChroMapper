8aa3f5443e2d50835854a3462f09bf4ca707de31
Caeden Statia <caeden.s@outlook.com>
2020-01-18 19:56:23 -0800

    fix bugs + rotating lasers now symmetrical agane

--- Assets/__Scripts/Platforms/RotatingLights.cs
+++ Assets/__Scripts/Platforms/RotatingLights.cs
@@ -5,8 +5,8 @@ public class RotatingLights : MonoBehaviour {
     private int speed;
     private Vector3 rotationVector = Vector3.up;
 
-    private float multiplier = 20;
-    private float rotationSpeed = 0;
+    [SerializeField] private float multiplier = 20;
+    [SerializeField] private float rotationSpeed = 0;
     private Quaternion startRotation;
 
     private void Start()
@@ -19,14 +19,14 @@ public class RotatingLights : MonoBehaviour {
         transform.Rotate(rotationVector, Time.deltaTime * rotationSpeed, Space.Self);
     }
 
-    public void UpdateOffset(int Speed, float Rotation, bool RotateBackwards)
+    public void UpdateOffset(int Speed, float Rotation)
     {
         speed = Speed;
         transform.rotation = startRotation;
         if (Speed > 0)
         {
             transform.Rotate(rotationVector, Rotation, Space.Self);
-            rotationSpeed = speed * multiplier * (RotateBackwards ? 1 : -1);
+            rotationSpeed = speed * multiplier;
         }
         else rotationSpeed = 0;
     }
