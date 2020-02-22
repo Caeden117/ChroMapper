bd79e83c2680596da64e466cbfa75b247b07d8e5
Caeden Statia <caeden.s@outlook.com>
2020-02-21 21:21:25 -0800

    Fixes with placement detection

--- Assets/__Scripts/MapEditor/UI/NoteLanesController.cs
+++ Assets/__Scripts/MapEditor/UI/NoteLanesController.cs
@@ -21,11 +21,22 @@ public class NoteLanesController : MonoBehaviour {
             noteGrid.localScale = new Vector3((float)noteLanes / 10 + 0.01f, 1, noteGrid.localScale.z);
             foreach(BoxCollider boxCollider in noteGrid.GetComponentsInChildren<BoxCollider>())
             {
+                //Here's a sort of hacky fix to get Notes not snapping outside the visual grid.
+                //For the interface grids (vertical), we want the entire box collider to shrink.
+                //For the horizontal grids, we can only scale down the sides, not forward/backward.
                 if (boxCollider.transform.GetComponent<PlacementMessageSender>() == null) continue;
                 float scaleX = 10 / noteGrid.localScale.x;
-                float scaleZ = 10 / boxCollider.transform.parent.localScale.z;
-                    boxCollider.size = new Vector3(scaleX * (noteGrid.localScale.x - 0.02f),
-                    boxCollider.size.y, scaleZ * (boxCollider.transform.parent.localScale.z - 0.2f));
+                Vector3 newSize = new Vector3(scaleX * (noteGrid.localScale.x - 0.02f), boxCollider.size.y, 0);
+                if (boxCollider.transform.parent.name.Contains("Interface"))
+                {
+                    float scaleZ = 10 / boxCollider.transform.parent.localScale.z;
+                    newSize += new Vector3(0, 0, scaleZ * (boxCollider.transform.parent.localScale.z - 0.2f));
+                }
+                else
+                {
+                    newSize += new Vector3(0, 0, boxCollider.size.z);
+                }
+                boxCollider.size = newSize;
             }
         }
     }
