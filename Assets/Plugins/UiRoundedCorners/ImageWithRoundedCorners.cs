562fc1bf3b15e0fa73be1435b0d833c249a04ec1
Ryan Ernest <ryan@rivergroup.com>
2020-02-28 21:29:08 -0500

    This prob wont completely fix the errors with Options but still test it.

--- Assets/Plugins/UiRoundedCorners/ImageWithRoundedCorners.cs
+++ Assets/Plugins/UiRoundedCorners/ImageWithRoundedCorners.cs
@@ -6,19 +6,28 @@ using UnityEngine.UI;
 public class ImageWithRoundedCorners : MonoBehaviour {
 	private static readonly int Props = Shader.PropertyToID("_WidthHeightRadius");
 
-	public Material material;
+	[SerializeField] private Material material;
+	[HideInInspector] public Material mat;
+	[Tooltip("When this is false the inserted material will change the inserted material shader.")] public bool cloneMaterial = true;
 	public float radius;
 
 	void OnRectTransformDimensionsChange(){
 		Refresh();
 	}
 	
-	private void OnValidate(){
+	private void OnValidate()
+	{
+		mat = cloneMaterial ? new Material(material) : material;
+		
+		Image i = GetComponent<Image>();
+		i.material = mat;
+		i.material.name = "Inherited From Round Corners";
+		
 		Refresh();
 	}
 
 	private void Refresh(){
 		var rect = ((RectTransform) transform).rect;
-		material.SetVector(Props, new Vector4(rect.width, rect.height, radius, 0));
+		mat.SetVector(Props, new Vector4(rect.width, rect.height, radius, 0));
 	}
 }
