0ee27d0c51f0353356f2e2c6c22ab948bc0b91e7
Caeden Statia <caeden.s@outlook.com>
2020-03-15 22:40:15 -0700

    Add dankness

---
+++ Assets/__Scripts/UI/OopsAllAprilFools.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OopsAllAprilFools : MonoBehaviour
+{
+    [SerializeField] private Sprite chooChooMapper;
+    [SerializeField] private Image source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DateTime now = DateTime.Now;
+        if (now.Month == 4 && now.Day == 1)
+        {
+            source.sprite = chooChooMapper;
+        }
+    }
+}
