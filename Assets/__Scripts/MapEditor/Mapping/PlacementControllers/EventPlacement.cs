635fab1ea53f2a3d13286046221b6c3d7f7dc29f
Caeden Statia <caeden.s@outlook.com>
2020-03-23 22:45:13 -0700

    Add custom field if it doesn't exist

--- Assets/__Scripts/MapEditor/Mapping/PlacementControllers/EventPlacement.cs
+++ Assets/__Scripts/MapEditor/Mapping/PlacementControllers/EventPlacement.cs
@@ -76,7 +76,14 @@ public class EventPlacement : PlacementController<MapEvent, BeatmapEventContaine
             if (propID >= 0)
             {
                 if (queuedData._customData is null) queuedData._customData = new SimpleJSON.JSONObject();
-                queuedData._customData["_propID"] = propID;
+                if (queuedData._customData["_propID"] is null)
+                {
+                    queuedData._customData.Add("_propID", propID);
+                }
+                else
+                {
+                    queuedData._customData["_propID"] = propID;
+                }
             }
             else queuedData._customData?.Remove("_propID");
         }
