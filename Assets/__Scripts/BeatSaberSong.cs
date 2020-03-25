66eeaceeff69015cd12c895cf68cf622962d367d
Caeden Statia <caeden.s@outlook.com>
2020-03-25 16:53:02 -0700

    improved customdata object cleaning

--- Assets/__Scripts/BeatSaberSong.cs
+++ Assets/__Scripts/BeatSaberSong.cs
@@ -186,12 +186,9 @@ public class BeatSaberSong
             contributors.DistinctBy(x => x.ToJSONNode().ToString()).ToList().ForEach(x => contributorArrayFUCKYOUGIT.Add(x.ToJSONNode()));
             json["_customData"]["_contributors"] = contributorArrayFUCKYOUGIT;
 
-            //BeatSaver schema changes, see below comment.
-            if (string.IsNullOrEmpty(customData["_editor"])) json["_customData"]["_editor"] = "chromapper";
-            if (string.IsNullOrEmpty(customData["_contributors"])) json["_customData"].Remove("_contributors");
-            if (string.IsNullOrEmpty(customData["_customEnvironment"])) json["_customData"].Remove("_customEnvironment");
-            if (string.IsNullOrEmpty(customData["_customEnvironmentHash"])) json["_customData"].Remove("_customEnvironmentHash");
-            if (json["_customData"].Linq.Count() <= 0) json.Remove("_customData");
+            //BeatSaver schema changes, CleanObject function
+            json["_customData"] = CleanObject(json["_customData"]);
+            if (json["_customData"] is null || json["_customData"].Count <= 0) json.Remove("_customData");
 
             JSONArray sets = new JSONArray();
             foreach (DifficultyBeatmapSet set in difficultyBeatmapSets)
@@ -234,20 +231,8 @@ public class BeatSaberSong
                      */
                     if (subNode["_customData"] != null)
                     {
-                        if (string.IsNullOrEmpty(diff.customData["_difficultyLabel"])) subNode["_customData"].Remove("_difficultyLabel");
-                        if (diff.customData["_editorOldOffset"] != null && diff.customData["_editorOldOffset"].AsFloat <= 0)
-                            subNode["_customData"].Remove("_editorOldOffset"); //For some reason these are used by MM but not by CM
-                        if (diff.customData["_editorOffset"] != null && diff.customData["_editorOffset"].AsFloat <= 0)
-                            subNode["_customData"].Remove("_editorOffset"); //So we're just gonna yeet them. Sorry squanksers.
-                        if (diff.customData["_warnings"] != null && diff.customData["_warnings"].AsArray.Count <= 0)
-                            subNode["_customData"].Remove("_warnings");
-                        if (diff.customData["_information"] != null && diff.customData["_information"].AsArray.Count <= 0)
-                            subNode["_customData"].Remove("_information");
-                        if (diff.customData["_suggestions"] != null && diff.customData["_suggestions"].AsArray.Count <= 0)
-                            subNode["_customData"].Remove("_suggestions");
-                        if (diff.customData["_requirements"] != null && diff.customData["_requirements"].AsArray.Count <= 0)
-                            subNode["_customData"].Remove("_requirements");
-                        if (subNode["_customData"].Linq.Count() <= 0) subNode.Remove("_customData");
+                        subNode["_customData"] = CleanObject(subNode["_customData"]);
+                        if (subNode["_customData"].Count <= 0) subNode.Remove("_customData");
                     }
                     else subNode.Remove("_customData"); //Just remove it if it's null lmao
 
@@ -269,6 +254,24 @@ public class BeatSaberSong
         {
             Debug.LogException(e);
         }
+    }
+
+    /// <summary>
+    /// Loops through all children of a JSON object, and remove any that are null or empty. 
+    /// This help makes _customData objects compliant with BeatSaver schema in a reusable and smart way.
+    /// </summary>
+    /// <param name="obj">Object of which to loop through and remove all empty children from.</param>
+    private JSONNode CleanObject(JSONNode obj)
+    {
+        if (obj is null) return null;
+        foreach (JSONNode node in obj)
+        {
+            if (node is null || node.AsArray?.Count <= 0 || string.IsNullOrEmpty(node.Value))
+            {
+                obj.Remove(node);
+            }
+        }
+        return obj;
     }
 
     public static BeatSaberSong GetSongFromFolder(string directory)
