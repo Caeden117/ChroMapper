a57938d58f3a6582ec6fbb2bad8461f9d1c3b717
Caeden Statia <caeden.s@outlook.com>
2020-04-11 23:26:34 -0700

    (unrelated) reflections are now PogU

--- Assets/__Scripts/MapEditor/ReflectionProbeSnapToY.cs
+++ Assets/__Scripts/MapEditor/ReflectionProbeSnapToY.cs
@@ -1,10 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ReflectionProbeSnapToY : MonoBehaviour
 {
+    private Camera mainCamera;
+    private PlatformDescriptor descriptor;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        LoadInitialMap.PlatformLoadedEvent += LoadPlatform;
+    }
+
+    private void LoadPlatform(PlatformDescriptor obj)
+    {
+        descriptor = obj;
+    }
+
     void Update()
     {
-        transform.localPosition = transform.parent.worldToLocalMatrix.MultiplyPoint(
-            new Vector3(transform.parent.position.x, -transform.parent.position.y * 1.5120233035103884909193591534643f, transform.parent.position.z));
+        if (descriptor is null) return;
+        Vector3 camDirWorld = mainCamera.transform.forward;
+        Vector3 camUpWorld = mainCamera.transform.up;
+        Vector3 camPosWorld = mainCamera.transform.position;
+
+        Vector3 camDirPlane = descriptor.transform.InverseTransformDirection(camDirWorld);
+        Vector3 camUpPlane = descriptor.transform.InverseTransformDirection(camUpWorld);
+        Vector3 camPosPlane = descriptor.transform.InverseTransformPoint(camPosWorld);
+
+        camDirPlane.y *= -1f;
+        camUpPlane.y *= -1f;
+        camPosPlane.y *= -1f;
+
+        camDirWorld = descriptor.transform.TransformDirection(camDirPlane);
+        camUpWorld = descriptor.transform.TransformDirection(camUpWorld);
+        camPosWorld = descriptor.transform.TransformPoint(camPosPlane);
+
+        transform.position = camPosWorld;
+        transform.LookAt(camPosWorld + camDirWorld, camUpWorld);
+    }
+
+    private void OnDestroy()
+    {
+        LoadInitialMap.PlatformLoadedEvent -= LoadPlatform;
     }
 }
