9d6c0469ef9549d797c18fdbf22a1cbe319a307f
Caeden117 <caeden.s@outlook.com>
2020-06-06 13:02:00 -0700

    Cleaned up Chroma converter

--- Assets/__Scripts/MapEditor/Mapping/LegacyNotesConverter.cs
+++ Assets/__Scripts/MapEditor/Mapping/LegacyNotesConverter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+//TODO rename to LegacyEventsConverter
 public class LegacyNotesConverter : MonoBehaviour {
 
     public void ConvertFrom()
@@ -19,7 +20,7 @@ public class LegacyNotesConverter : MonoBehaviour {
     private IEnumerator ConvertFromLegacy()
     {
         yield return PersistentUI.Instance.FadeInLoadingScreen();
-        //TODO convert legacy Chroma events to Chroma 2.0 events
+
         var events = BeatmapObjectContainerCollection.GetCollectionForType(BeatmapObject.Type.EVENT) as EventsContainer;
         Dictionary<int, Color?> chromaColorsByEventType = new Dictionary<int, Color?>();
         foreach (var obj in events.UnsortedObjects)
@@ -50,35 +51,17 @@ public class LegacyNotesConverter : MonoBehaviour {
                 chromaColorsByEventType.Add(e._type, null);
             }
         }
-        /*IEnumerable<IGrouping<int, MapEvent>> groupedByType = events.LoadedObjects.Cast<MapEvent>().GroupBy(x => x._type);
-        foreach (var grouping in groupedByType)
-        {
-            Color? currentChromaColor = null;
-            foreach (MapEvent e in grouping)
-            {
-                if (e._value >= ColourManager.RGB_INT_OFFSET)
-                {
-                    currentChromaColor = ColourManager.ColourFromInt(e._value);
-                    events.DeleteObject(e, false);
-                    continue;
-                }
-                else if (e._value == ColourManager.RGB_RESET)
-                {
-                    currentChromaColor = null;
-                    events.DeleteObject(e, false);
-                    continue;
-                }
-                if (currentChromaColor != null && e._value != MapEvent.LIGHT_VALUE_OFF)
-                {
-                    if (e._customData == null) e._customData = new JSONObject();
-                    e._customData["_color"] = currentChromaColor;
-                }
-            }
-        }*/
         events.RefreshPool(true);
+
         yield return PersistentUI.Instance.FadeOutLoadingScreen();
     }
 
+    /*
+     * I've ignored the ability to convert from Chroma 2.0 back to 1.0 since I do not see any use for doing that,
+     * other than for perhaps Quest users stuck using the ChromaLite mod.
+     * 
+     * If given enough demand, or perhaps a PR, I'll add it.
+     */
     private IEnumerator ConvertToLegacy()
     {
         yield return PersistentUI.Instance.FadeInLoadingScreen();
