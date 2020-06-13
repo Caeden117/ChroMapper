c85510a3c657af30b9d8a0e15c65bb7a5c55e048
Caeden117 <caeden.s@outlook.com>
2020-06-12 21:56:18 -0700

    Restore SelectObjectOnGrid, fix spectrogram rendering issue

--- Assets/__Scripts/MapEditor/Mapping/Selection/SelectObjectOnGrid.cs
+++ Assets/__Scripts/MapEditor/Mapping/Selection/SelectObjectOnGrid.cs
@@ -3,80 +3,57 @@ using System.Linq;
 using UnityEngine;
 
 public class SelectObjectOnGrid : MonoBehaviour {
-    //TODO is this needed?
-    /*[SerializeField] private NotesContainer notesContainer;
-    [SerializeField] private EventsContainer eventsContainer;
 
     void Start()
     {
         SelectionController.ObjectWasSelectedEvent += ObjectSelected;
     }
 
-    void ObjectSelected(BeatmapObjectContainer container)
+    void ObjectSelected(BeatmapObject container)
     {
         if (!KeybindsController.CtrlHeld || Settings.Instance.BoxSelect) return; //Only do mass selection on ctrl
         if (SelectionController.SelectedObjects.Count() < 2 && !KeybindsController.AltHeld) return;
-        List<BeatmapObjectContainer> containers = new List<BeatmapObjectContainer>(SelectionController.SelectedObjects);
-        List<BeatmapEventContainer> events = containers.Where(x => x is BeatmapEventContainer).Cast<BeatmapEventContainer>().ToList(); //Filter containers
-        List<BeatmapNoteContainer> notes = containers.Where(x => x is BeatmapNoteContainer).Cast<BeatmapNoteContainer>().ToList();
-        Debug.Log("MASS SELECT TIME");
-        if (events.Count > 0)
+        IEnumerable<MapEvent> events = SelectionController.SelectedObjects.Where(x => x is MapEvent).Cast<MapEvent>();
+        IEnumerable<BeatmapNote> notes = SelectionController.SelectedObjects.Where(x => x is BeatmapNote).Cast<BeatmapNote>();
+        if (events.Any())
         {
-            events = events.OrderBy(x => x.eventData._type).ThenByDescending(x => x.objectData._time).ToList();
+            events = events.OrderBy(x => x._type).ThenByDescending(x => x._time);
             for (var i = 0; i < 15; i++)
             {
-                List<BeatmapEventContainer> eventsAtType = events.Where(x => x.eventData._type == i).ToList();
-                if (eventsAtType.Count >= 2)
+                IEnumerable<MapEvent> eventsAtType = events.Where(x => x._type == i);
+                if (eventsAtType.Count() >= 2)
                 {
-                    List<BeatmapObjectContainer> inBetween = eventsContainer.LoadedObjects.Where(x =>
-                        x.objectData._time >= eventsAtType.Last().objectData._time &&
-                        x.objectData._time <= eventsAtType.First().objectData._time &&
-                        (x.objectData as MapEvent)._type == i).ToList();
-                    foreach (BeatmapObjectContainer con in inBetween) SelectionController.Select(con, true, false);
+                    IEnumerable<BeatmapObject> inBetween = BeatmapObjectContainerCollection.GetCollectionForType(BeatmapObject.Type.EVENT)
+                        .LoadedObjects.Where(x =>
+                        x._time >= eventsAtType.Last()._time &&
+                        x._time <= eventsAtType.First()._time &&
+                        (x as MapEvent)._type == i);
+                    foreach (BeatmapObject con in inBetween) SelectionController.Select(con, true, false, false);
                 }
             }
         }
-        if (notes.Count > 0)
+        if (notes.Any())
         {
-            notes = notes.OrderBy(x => x.mapNoteData._lineIndex).ThenByDescending(x => x.objectData._time).ToList();
-            for (var i = notes.First().mapNoteData._lineIndex; i <= notes.Last().mapNoteData._lineIndex; i++)
+            notes = notes.OrderBy(x => x._lineIndex).ThenByDescending(x => x._time);
+            for (var i = notes.First()._lineIndex; i <= notes.Last()._lineIndex; i++)
             {
-                List<BeatmapNoteContainer> notesAtIndex = notes.Where(x => x.mapNoteData._lineIndex == i).ToList();
-                if (notesAtIndex.Count >= 2)
+                IEnumerable<BeatmapNote> notesAtIndex = notes.Where(x => x._lineIndex == i);
+                if (notesAtIndex.Count() >= 2)
                 {
-                    List<BeatmapObjectContainer> inBetween = notesContainer.LoadedObjects.Where(x =>
-                        x.objectData._time >= notesAtIndex.Last().objectData._time &&
-                        x.objectData._time <= notesAtIndex.First().objectData._time &&
-                        ((BeatmapNote) x.objectData)._lineIndex == i).ToList();
-                    foreach (BeatmapObjectContainer con in inBetween) SelectionController.Select(con, true, false);
+                    IEnumerable<BeatmapObject> inBetween = BeatmapObjectContainerCollection.GetCollectionForType(BeatmapObject.Type.NOTE)
+                        .LoadedObjects.Where(x =>
+                        x._time >= notesAtIndex.Last()._time &&
+                        x._time <= notesAtIndex.First()._time &&
+                        ((BeatmapNote)x)._lineIndex == i);
+                    foreach (BeatmapObject con in inBetween) SelectionController.Select(con, true, false, false);
                 }
             }
         }
         SelectionController.RefreshSelectionMaterial();
     }
 
-    private void Update()
-    {
-        /*
-         * While this may be a copy of PlacementController code and BeatmapObjectContainer selecting, this is actually to help
-         * select objects that are under the placement grid (such as walls)
-         * /
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, 999f, 1 << 11))
-        {
-            if (Physics.Raycast(ray, out _, hit.distance, 1 << 9)) return; //Do not select under grid if something is already selected.
-            Ray gridRay = new Ray(hit.point, ray.direction);
-            if (Physics.Raycast(gridRay, out RaycastHit gridHit, 999f, 1 << 9))
-            {
-                BeatmapObjectContainer con = gridHit.transform.gameObject.GetComponent<BeatmapObjectContainer>();
-                if (con is null) return;
-                //con.OnMouseOver();
-            }
-        }
-    }
-
     private void OnDestroy()
     {
         SelectionController.ObjectWasSelectedEvent -= ObjectSelected;
-    }*/
+    }
 }
