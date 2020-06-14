8c0fd6154d71b14323645e5cf89f4db3955b2589
Caeden117 <caeden.s@outlook.com>
2020-06-14 12:02:12 -0700

    cleaned up rotate lights code

--- Assets/__Scripts/Platforms/RotatingLights.cs
+++ Assets/__Scripts/Platforms/RotatingLights.cs
@@ -30,25 +30,31 @@ public class RotatingLights : MonoBehaviour {
         transform.Rotate(rotationVector, Time.deltaTime * rotationSpeed, Space.Self);
     }
 
+    // If you have any complaints about CM's inaccurate lasers, please look through this and tell me what the hell is wrong.
     public void UpdateOffset(int Speed, float Rotation, bool RotateForwards, JSONNode customData = null)
     {
         speed = Speed;
-        transform.localRotation = startRotation;
         bool lockRotation = false;
-        if (customData != null)
+        if (customData != null) //We have custom data in this event
         {
-            lockRotation = customData["_lockPosition"] ?? false;
-            speed = customData["_preciseSpeed"] ?? Speed;
-            RotateForwards = customData["_direction"]?.AsInt == 0;
+            lockRotation = customData["_lockPosition"] ?? false; //We lock position if it tells us to or not
+            speed = customData["_preciseSpeed"] ?? Speed; //We control precise speed if it tells us to or not
+            RotateForwards = customData["_direction"]?.AsInt == 0; //We rotate CW/CCW if it tells us to or not
         }
-        if (UseZPositionForAngleOffset && !lockRotation)
+        if (!lockRotation) //If we are not locking rotation, reset it to its default.
+        {
+            transform.localRotation = startRotation;
+        }
+        if (UseZPositionForAngleOffset && !lockRotation) //Timbaland has laser speeds offset by their Z position
         {
             Rotation = Time.frameCount + (transform.position.z * zPositionModifier);
         }
+        //Rotate by Rotation variable
+        //In most cases, it is randomized, except in Timbaland environment (see above)
         if (!lockRotation && speed > 0)
         {
             transform.Rotate(rotationVector, Rotation, Space.Self);
         }
-        rotationSpeed = speed * multiplier * (RotateForwards ? 1 : -1);
+        rotationSpeed = speed * multiplier * (RotateForwards ? 1 : -1); //Set rotation speed
     }
 }
