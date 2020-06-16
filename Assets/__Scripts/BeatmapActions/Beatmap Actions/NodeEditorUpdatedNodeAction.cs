ad8f08579bbefac84e39eea694ecfab4551b207a
Caeden117 <caeden.s@outlook.com>
2020-06-15 18:32:58 -0700

    fix NodeEditorUpdatedNodeAction

--- Assets/__Scripts/BeatmapActions/Beatmap Actions/NodeEditorUpdatedNodeAction.cs
+++ Assets/__Scripts/BeatmapActions/Beatmap Actions/NodeEditorUpdatedNodeAction.cs
@@ -18,17 +18,19 @@ public class NodeEditorUpdatedNodeAction : BeatmapAction
 
     public override void Undo(BeatmapActionContainer.BeatmapActionParams param)
     {
-        collection.DeleteObject(editedData, false, false);
-        JSONNode node = originalData.ConvertToJSON();
-        BeatmapObject newObject = Activator.CreateInstance(originalData.GetType(), new object[] { node }) as BeatmapObject;
-        collection.SpawnObject(newObject, false);
+        collection.DeleteObject(editedData, false);
+        SelectionController.Deselect(editedData);
+
+        collection.SpawnObject(originalData, true);
+        SelectionController.Select(originalData, false, true, false);
     }
 
     public override void Redo(BeatmapActionContainer.BeatmapActionParams param)
     {
-        collection.DeleteObject(originalData, false, false);
-        JSONNode node = editedData.ConvertToJSON();
-        BeatmapObject newObject = Activator.CreateInstance(originalData.GetType(), new object[] { node }) as BeatmapObject;
-        collection.SpawnObject(newObject, false);
+        collection.DeleteObject(originalData, false);
+        SelectionController.Deselect(originalData);
+
+        collection.SpawnObject(editedData, true);
+        SelectionController.Select(editedData, false, true, false);
     }
 }
