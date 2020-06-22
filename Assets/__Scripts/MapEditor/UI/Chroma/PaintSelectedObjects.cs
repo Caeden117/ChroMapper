0c1fd0c1d4a8853a1c3f65e28f7a09c2818ac8ce
Caeden117 <caeden.s@outlook.com>
2020-06-21 22:30:17 -0700

    Add Paintbrush

---
+++ Assets/__Scripts/MapEditor/UI/Chroma/PaintSelectedObjects.cs
@@ -0,0 +1,24 @@
+﻿using SimpleJSON;
+using System.Linq;
+using UnityEngine;
+
+public class PaintSelectedObjects : MonoBehaviour
+{
+    [SerializeField] private ColorPicker picker;
+
+    public void Paint()
+    {
+        foreach (BeatmapObject obj in SelectionController.SelectedObjects)
+        {
+            if (obj._customData.Children.Count() == 0) //TODO: Look into making BeatmapObject._customData nullable
+            {
+                obj._customData = new JSONObject();
+            }
+            obj._customData.Add("_color", picker.CurrentColor);
+        }
+        foreach (BeatmapObject unique in SelectionController.SelectedObjects.DistinctBy(x => x.beatmapType))
+        {
+            BeatmapObjectContainerCollection.GetCollectionForType(unique.beatmapType).RefreshPool(true);
+        }
+    }
+}
