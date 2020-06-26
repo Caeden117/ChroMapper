e82c2d0392b17da79419250286205cc6f6410023
Caeden117 <caeden.s@outlook.com>
2020-06-26 00:48:57 -0700

    Improvements to paint tool

--- Assets/__Scripts/MapEditor/UI/Chroma/PaintSelectedObjects.cs
+++ Assets/__Scripts/MapEditor/UI/Chroma/PaintSelectedObjects.cs
@@ -11,11 +11,28 @@ public class PaintSelectedObjects : MonoBehaviour
         foreach (BeatmapObject obj in SelectionController.SelectedObjects)
         {
             if (obj is BeatmapBPMChange || obj is BeatmapCustomEvent) continue; //These should probably not be colored.
+            if (obj is MapEvent @event)
+            {
+                if (@event._value == MapEvent.LIGHT_VALUE_OFF) continue; //Ignore painting Off events
+                if (@event._lightGradient != null)
+                { 
+                    //Modify start color if we are painting a Chroma 2.0 gradient
+                    @event._lightGradient.StartColor = picker.CurrentColor;
+                    continue;
+                }
+            }
             if (obj._customData.Children.Count() == 0) //TODO: Look into making BeatmapObject._customData nullable
             {
                 obj._customData = new JSONObject();
             }
-            obj._customData.Add("_color", picker.CurrentColor);
+            if (!obj._customData.HasKey("_color"))
+            {
+                obj._customData.Add("_color", picker.CurrentColor);
+            }
+            else
+            {
+                obj._customData["_color"] = picker.CurrentColor;
+            }
         }
         foreach (BeatmapObject unique in SelectionController.SelectedObjects.DistinctBy(x => x.beatmapType))
         {
