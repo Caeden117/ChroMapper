4a657c38a4247284f95925268e1df4c1f43e2efe
Caeden117 <caeden.s@outlook.com>
2020-07-28 23:17:20 -0700

    Move extension classes to folder

---
+++ Assets/__Scripts/Extensions/CharExtensions.cs
@@ -0,0 +1,9 @@
+﻿static class CharExtensions
+{
+    public static bool IsHex(this char c)
+    {
+        return (c >= '0' && c <= '9') ||
+                 (c >= 'a' && c <= 'f') ||
+                 (c >= 'A' && c <= 'F');
+    }
+}
