7f40accda39c5296950565cc08633ee61fe2ba1c
Caeden117 <caeden.s@outlook.com>
2020-09-03 14:15:24 -0700

    AAAAAAAAAAA FIXED NOTE DIRECTIONS

--- Assets/__Scripts/MapEditor/Mapping/PlacementControllers/NotePlacement.cs
+++ Assets/__Scripts/MapEditor/Mapping/PlacementControllers/NotePlacement.cs
@@ -1,6 +1,8 @@
 ﻿using SimpleJSON;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,10 +11,21 @@ public class NotePlacement : PlacementController<BeatmapNote, BeatmapNoteContain
     [SerializeField] private NoteAppearanceSO noteAppearanceSO;
     [SerializeField] private DeleteToolController deleteToolController;
     [SerializeField] private PrecisionPlacementGridController precisionPlacement;
-    private bool upNote = false;
-    private bool leftNote = false;
-    private bool downNote = false;
-    private bool rightNote = false;
+
+    private bool diagonal = false;
+    private bool flagDirectionsUpdate = false;
+
+    private const int UpKey = 0;
+    private const int LeftKey = 1;
+    private const int DownKey = 2;
+    private const int RightKey = 3;
+
+    // REVIEW: Perhaps partner with Obama to turn this list of bools
+    // into some binary shifting goodness
+    private List<bool> HeldKeys = new List<bool>() { false, false, false, false };
+
+    // TODO: Perhaps move this into Settings as a user-configurable option
+    private readonly float DIAGONAL_STICK_MAX_TIME = 0.3f; // This controls the maximum time that a note will stay a diagonal
 
     // Chroma Color Stuff
     public static readonly string ChromaColorKey = "PlaceChromaObjects";
@@ -197,37 +210,36 @@ public class NotePlacement : PlacementController<BeatmapNote, BeatmapNoteContain
     //TODO perhaps make a helper function to deal with the context.performed and context.canceled checks
     public void OnDownNote(InputAction.CallbackContext context)
     {
-        if (context.performed ^ context.canceled)
-        {
-            downNote = context.performed;
-            HandleDirectionValues();
-        }
+        HandleKeyUpdate(context, DownKey);
     }
 
     public void OnLeftNote(InputAction.CallbackContext context)
     {
-        if (context.performed ^ context.canceled)
-        {
-            leftNote = context.performed;
-            HandleDirectionValues();
-        }
+        HandleKeyUpdate(context, LeftKey);
     }
 
     public void OnUpNote(InputAction.CallbackContext context)
     {
-        if (context.performed ^ context.canceled)
-        {
-            upNote = context.performed;
-            HandleDirectionValues();
-        }
+        HandleKeyUpdate(context, UpKey);
     }
 
     public void OnRightNote(InputAction.CallbackContext context)
     {
-        if (context.performed ^ context.canceled)
+        HandleKeyUpdate(context, RightKey);
+    }
+
+    private void HandleKeyUpdate(InputAction.CallbackContext context, int id)
+    {
+        if (context.performed ^ HeldKeys[id]) flagDirectionsUpdate = true;
+        HeldKeys[id] = context.performed;
+    }
+
+    private void LateUpdate()
+    {
+        if (flagDirectionsUpdate)
         {
-            rightNote = context.performed;
             HandleDirectionValues();
+            flagDirectionsUpdate = false;
         }
     }
 
@@ -235,8 +247,22 @@ public class NotePlacement : PlacementController<BeatmapNote, BeatmapNoteContain
     {
         deleteToolController.UpdateDeletion(false);
 
+        bool upNote = HeldKeys[UpKey];
+        bool downNote = HeldKeys[DownKey];
+        bool leftNote = HeldKeys[LeftKey];
+        bool rightNote = HeldKeys[RightKey];
+        bool previousDiagonalState = diagonal;
+
         bool handleUpDownNotes = upNote ^ downNote; // XOR: True if the values are different, false if the same
         bool handleLeftRightNotes = leftNote ^ rightNote;
+
+        diagonal = handleUpDownNotes && handleLeftRightNotes;
+
+        if (previousDiagonalState == true && diagonal == false)
+        {
+            StartCoroutine(CheckForDiagonalUpdate());
+            return;
+        }
 
         if (handleUpDownNotes && !handleLeftRightNotes) // We handle simple up/down notes
         {
@@ -247,8 +273,9 @@ public class NotePlacement : PlacementController<BeatmapNote, BeatmapNoteContain
         {
             if (leftNote) UpdateCut(BeatmapNote.NOTE_CUT_DIRECTION_LEFT);
             else UpdateCut(BeatmapNote.NOTE_CUT_DIRECTION_RIGHT);
+
         }
-        else if (handleUpDownNotes && handleLeftRightNotes) //We need to do a diagonal
+        else if (diagonal) //We need to do a diagonal
         {
             if (leftNote)
             {
@@ -260,6 +287,17 @@ public class NotePlacement : PlacementController<BeatmapNote, BeatmapNoteContain
                 if (upNote) UpdateCut(BeatmapNote.NOTE_CUT_DIRECTION_UP_RIGHT);
                 else UpdateCut(BeatmapNote.NOTE_CUT_DIRECTION_DOWN_RIGHT);
             }
+        }
+    }
+
+    private IEnumerator CheckForDiagonalUpdate()
+    {
+        List<bool> previousHeldKeys = new List<bool>(HeldKeys);
+        yield return new WaitForSeconds(DIAGONAL_STICK_MAX_TIME);
+        // Weird way of saying "Are the keys being held right now the same as before"
+        if (!previousHeldKeys.Except(HeldKeys).Any())
+        {
+            flagDirectionsUpdate = true;
         }
     }
 
