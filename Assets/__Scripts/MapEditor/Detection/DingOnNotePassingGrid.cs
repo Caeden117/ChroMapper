fb825017f8513779c51e56042383de9d9fad4c58
Caeden117 <caeden.s@outlook.com>
2020-09-29 16:34:35 -0700

    add rabbit viewer tick sound

--- Assets/__Scripts/MapEditor/Detection/DingOnNotePassingGrid.cs
+++ Assets/__Scripts/MapEditor/Detection/DingOnNotePassingGrid.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using __Scripts.MapEditor.Hit_Sounds;
 using UnityEngine;
 
 public class DingOnNotePassingGrid : MonoBehaviour {
--- Assets/__Scripts/MapEditor/Hit Sounds/HitSounds.cs
+++ Assets/__Scripts/MapEditor/Hit Sounds/HitSounds.cs
@@ -1,16 +1,15 @@
-﻿namespace __Scripts.MapEditor.Hit_Sounds
+﻿
+public enum HitSounds
 {
-    public enum HitSounds
-    {
-        TICK = 0,
-        SLICE = 1,
-        QUACK = 2,
-        MEOW = 3,
-        RAWR = 4,
-        WOOF = 5,
-        OINK = 6,
-        TWEET = 7,
-        BASS_BOOST = 8,
-        DISCORD = 9
-    }
+    TICK = 0,
+    SLICE = 1,
+    QUACK = 2,
+    MEOW = 3,
+    RAWR = 4,
+    WOOF = 5,
+    OINK = 6,
+    TWEET = 7,
+    BASS_BOOST = 8,
+    DISCORD = 9,
+    RABBITS_VIEWER = 10
 }
