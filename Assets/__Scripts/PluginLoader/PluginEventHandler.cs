497c6e8a738bccb2c1d81b18ae81e571b2d766a6
Thomas Cheyney <top_cat@thomasc.co.uk>
2020-11-25 01:53:06 +0000

    Add note event too

--- Assets/__Scripts/PluginLoader/PluginEventHandler.cs
+++ Assets/__Scripts/PluginLoader/PluginEventHandler.cs
@@ -8,16 +8,23 @@ class PluginEventHandler : MonoBehaviour
     private void Awake()
     {
         interfaceCallback.EventPassedThreshold += EventPassedThreshold;
+        interfaceCallback.NotePassedThreshold += NotePassedThreshold;
     }
 
     private void OnDestroy()
     {
         interfaceCallback.EventPassedThreshold -= EventPassedThreshold;
+        interfaceCallback.NotePassedThreshold -= NotePassedThreshold;
     }
 
     private void EventPassedThreshold(bool _, int __, BeatmapObject newlyAdded)
     {
         PluginLoader.BroadcastEvent<EventPassedThresholdAttribute, BeatmapObject>(newlyAdded);
+    }
+
+    private void NotePassedThreshold(bool _, int __, BeatmapObject newlyAdded)
+    {
+        PluginLoader.BroadcastEvent<NotePassedThresholdAttribute, BeatmapObject>(newlyAdded);
     }
 
 }
