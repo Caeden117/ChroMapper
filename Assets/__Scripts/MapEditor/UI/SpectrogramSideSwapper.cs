e2a5bc70d4e30c2b6eafb2d7a779073841f02c4e
dcljr <dcljr@me.com>
2020-12-06 19:58:06 -0800

    Added swap spectrogram side button

---
+++ Assets/__Scripts/MapEditor/UI/SpectrogramSideSwapper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SpectrogramSideSwapper : MonoBehaviour
+{
+    public bool IsNoteSide { get; set; } = true;
+
+    [SerializeField] private GridChild spectrogramGridChild;
+    [SerializeField] private GridChild spectrogramChunksChild;
+
+    public void SwapSides()
+    {
+        IsNoteSide = !IsNoteSide;
+
+        int order = IsNoteSide ? -1 : 3;
+        float offset = IsNoteSide ? 3.5f : 2.5f;
+
+        GridOrderController.DeregisterChild(spectrogramChunksChild);
+        GridOrderController.DeregisterChild(spectrogramGridChild);
+        if (Settings.Instance.WaveformWorkflow)
+        {
+            spectrogramChunksChild.Order = spectrogramGridChild.Order = order;
+            spectrogramGridChild.LocalOffset = new Vector3(offset, 0, 0);
+            spectrogramChunksChild.LocalOffset = new Vector3(offset - 2, 0, 0);
+        }
+        GridOrderController.RegisterChild(spectrogramChunksChild);
+        GridOrderController.RegisterChild(spectrogramGridChild);
+    }
+}
