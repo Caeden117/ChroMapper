bc87cf110f2b6411d0b5899c018d2e960a42342c
Caeden117 <caeden.s@outlook.com>
2021-06-15 22:17:07 -0700

    added some haha funnies to interscope

---
+++ Assets/__Scripts/Platforms/Interscope/ToggleObjectsOnMouseClick.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ToggleObjectsOnMouseClick : MonoBehaviour
+{
+    [SerializeField] private GameObject[] shitToToggle;
+
+    private bool objectsEnabled = false;
+
+    private void Start()
+    {
+        foreach (var go in shitToToggle) go.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        objectsEnabled = !objectsEnabled;
+
+        foreach (var go in shitToToggle) go.SetActive(objectsEnabled);
+    }
+}
