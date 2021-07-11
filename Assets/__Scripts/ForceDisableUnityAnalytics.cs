ac05033f7be8cfb14a130d6ecd10409e110dc58e
Caeden117 <caeden.s@outlook.com>
2021-07-11 15:53:16 -0700

    Force disable Unity Analytics

---
+++ Assets/__Scripts/ForceDisableUnityAnalytics.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Analytics;
+
+public class ForceDisableUnityAnalytics : MonoBehaviour
+{
+    private void Awake()
+    {
+        Analytics.enabled = false;
+        Analytics.deviceStatsEnabled = false;
+        PerformanceReporting.enabled = false;
+    }
+}
