aa31522a480502a9d1e7b598cdc15b490bee7084
Thomas Cheyney <top_cat@thomasc.co.uk>
2021-07-18 17:04:01 +0100

    Bop unity analytics

--- Assets/__Scripts/ForceDisableUnityAnalytics.cs
+++ Assets/__Scripts/ForceDisableUnityAnalytics.cs
@@ -5,6 +5,7 @@ public class ForceDisableUnityAnalytics : MonoBehaviour
 {
     private void Awake()
     {
+        Analytics.initializeOnStartup = false;
         Analytics.enabled = false;
         Analytics.deviceStatsEnabled = false;
         PerformanceReporting.enabled = false;
