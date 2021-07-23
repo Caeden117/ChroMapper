64d0a8f864f66b4215b86e2c4b90beffe2ccfbfa
GalaxyMaster2 <10052298+GalaxyMaster2@users.noreply.github.com>
2021-07-23 22:59:34 +0200

    add keybind to toggle-mute hitsounds

---
+++ Assets/__Scripts/MapEditor/Hit Sounds/HitSoundVolumeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.__Scripts.MapEditor.Hit_Sounds
+{
+    class HitSoundVolumeController : MonoBehaviour, CMInput.IAudioActions
+    {
+        [SerializeField] private float lastVolume = 0f;
+
+        public void OnToggleHitsoundMute(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+
+            float currentVolume = Settings.Instance.NoteHitVolume;
+            if (currentVolume == 0f)
+            {
+                Settings.Instance.NoteHitVolume = lastVolume;
+            }
+            else
+            {
+                lastVolume = currentVolume;
+                Settings.Instance.NoteHitVolume = 0f;
+            }
+        }
+    }
+}
