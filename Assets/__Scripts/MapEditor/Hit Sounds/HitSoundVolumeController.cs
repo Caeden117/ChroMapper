fb44ece83f7cf5e38555385cb1ad0bc0402bec46
GalaxyMaster2 <10052298+GalaxyMaster2@users.noreply.github.com>
2021-07-24 12:33:41 +0200

    remove namespace

--- Assets/__Scripts/MapEditor/Hit Sounds/HitSoundVolumeController.cs
+++ Assets/__Scripts/MapEditor/Hit Sounds/HitSoundVolumeController.cs
@@ -1,26 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Assets.__Scripts.MapEditor.Hit_Sounds
+public class HitSoundVolumeController : MonoBehaviour, CMInput.IAudioActions
 {
-    class HitSoundVolumeController : MonoBehaviour, CMInput.IAudioActions
+    [SerializeField] private float lastVolume = 0f;
+
+    public void OnToggleHitsoundMute(InputAction.CallbackContext context)
     {
-        [SerializeField] private float lastVolume = 0f;
+        if (!context.performed) return;
 
-        public void OnToggleHitsoundMute(InputAction.CallbackContext context)
+        float currentVolume = Settings.Instance.NoteHitVolume;
+        if (currentVolume == 0f)
         {
-            if (!context.performed) return;
-
-            float currentVolume = Settings.Instance.NoteHitVolume;
-            if (currentVolume == 0f)
-            {
-                Settings.Instance.NoteHitVolume = lastVolume;
-            }
-            else
-            {
-                lastVolume = currentVolume;
-                Settings.Instance.NoteHitVolume = 0f;
-            }
+            Settings.Instance.NoteHitVolume = lastVolume;
+        }
+        else
+        {
+            lastVolume = currentVolume;
+            Settings.Instance.NoteHitVolume = 0f;
         }
     }
 }
