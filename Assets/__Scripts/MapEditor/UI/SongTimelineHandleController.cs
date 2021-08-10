3032ecde7b3af2d4b1106f61653e49f5491cf651
GalaxyMaster2 <10052298+GalaxyMaster2@users.noreply.github.com>
2021-08-10 22:11:20 +0200

    enable timeline dragging during playback

--- Assets/__Scripts/MapEditor/UI/SongTimelineHandleController.cs
+++ Assets/__Scripts/MapEditor/UI/SongTimelineHandleController.cs
@@ -4,15 +4,45 @@ using UnityEngine.EventSystems;
 public class SongTimelineHandleController : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
 {
     [SerializeField] private SongTimelineController timeline;
+    [SerializeField] private AudioTimeSyncController atsc;
+    private bool resume = false;
+
+    private void OnEnable()
+    {
+        atsc.OnPlayToggle += OnPlayToggle;
+    }
 
     public void OnPointerDown(PointerEventData fucku)
     {
+        if (atsc.IsPlaying)
+        {
+            atsc.TogglePlaying();
+            resume = true;
+        }
+
         timeline.IsClicked = true;
+        timeline.TriggerUpdate();
     }
 
     public void OnPointerUp(PointerEventData fucku)
     {
         timeline.TriggerUpdate();
         timeline.IsClicked = false;
+
+        if (resume && !atsc.IsPlaying)
+        {
+            atsc.TogglePlaying();
+        }
+        resume = false;
+    }
+
+    private void OnPlayToggle(bool playing)
+    {
+        resume = false;
+    }
+
+    private void OnDestroy()
+    {
+        atsc.OnPlayToggle -= OnPlayToggle;
     }
 }
