3d816f172a7a6337340f3e8f77a5e9156ab79187
Bullet <vincentn@live.com.au>
2022-05-13 19:59:30 +1000

    Add difficulty name display in editor

---
+++ Assets/__Scripts/MapEditor/UI/CurrentDifficultyDisplay.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class CurrentDifficultyDisplay : MonoBehaviour
+{
+    private TextMeshProUGUI textMesh;
+
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        BeatSaberSong.DifficultyBeatmap data = BeatSaberSongContainer.Instance.DifficultyData;
+        string diffStr = data.Difficulty;
+        textMesh.text = (data.CustomData != null && data.CustomData.HasKey("_difficultyLabel"))
+            ? $"{data.CustomData["_difficultyLabel"].Value} - [{diffStr}]"
+            : $"[{diffStr}]";
+    }
+}
