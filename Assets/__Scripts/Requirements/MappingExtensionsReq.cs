93bdfa75d259c0d340cddfe9b7036f1f728c133c
Bullet <vincentn@live.com.au>
2022-07-19 22:13:29 +1000

    Update ME requirement check for V3 objects

--- Assets/__Scripts/Requirements/MappingExtensionsReq.cs
+++ Assets/__Scripts/Requirements/MappingExtensionsReq.cs
@@ -13,9 +13,12 @@ public class MappingExtensionsReq : RequirementCheck
                    (note.LineIndex < 0 || note.LineIndex > 3 || note.LineLayer < 0 || note.LineLayer > 2) &&
                    note.CustomData.Count <= 0) ||
                map.Obstacles.Any(ob =>
-                   (ob.LineIndex < 0 || ob.LineIndex > 3 || ob.Type >= 2 || ob.Width >= 1000) &&
+                   (!(ob is BeatmapObstacleV3) && (ob.LineIndex < 0 || ob.LineIndex > 3)
+                   || (ob is BeatmapObstacleV3) && (ob.LineIndex <= -1000 || ob.LineIndex >= 1000)
+                   || ob.Type >= 2 || ob.Width >= 1000) &&
                    ob.CustomData.Count <= 0) ||
-               map.Events.Any(ob => ob.IsRotationEvent && ob.Value >= 1000 && ob.Value <= 1720)
+               map.Events.Any(ob => ob.IsRotationEvent && !(ob is RotationEvent) // V3 rotation
+                    && ob.Value >= 1000 && ob.Value <= 1720)
             ? RequirementType.Requirement
             : RequirementType.None;
     }
