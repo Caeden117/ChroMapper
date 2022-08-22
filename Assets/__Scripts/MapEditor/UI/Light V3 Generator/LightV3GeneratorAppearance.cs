5c53f5471781a3f8d82c55af71de0c7cd45213d4
CCDV2 <denghaoyuan1100@126.com>
2022-08-22 14:56:45 +0800

    now light v3 UI can be toggled after click

---
+++ Assets/__Scripts/MapEditor/UI/Light V3 Generator/LightV3GeneratorAppearance.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// basically similar to <see cref="StrobeGeneratorUIDropdown"/>
+/// </summary>
+public class LightV3GeneratorAppearance : MonoBehaviour
+{
+    [SerializeField] private RectTransform lightV3GenUIRect;
+
+    public bool IsActive { get; private set; }
+
+    public void ToggleDropdown(bool visible) => StartCoroutine(UpdateGroup(visible, lightV3GenUIRect));
+
+    private IEnumerator UpdateGroup(bool enabled, RectTransform group)
+    {
+        IsActive = enabled;
+        float dest = enabled ? -150 : 120;
+        var og = group.anchoredPosition.x;
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            group.anchoredPosition = new Vector2(Mathf.Lerp(og, dest, t), group.anchoredPosition.y);
+            og = group.anchoredPosition.x;
+            yield return new WaitForEndOfFrame();
+        }
+
+        group.anchoredPosition = new Vector2(dest, group.anchoredPosition.y);
+    }
+}
