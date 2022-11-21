c3a1762829f610d341c7863229b5526825297f4f
CCDV2 <denghaoyuan1100@126.com>
2022-11-22 01:34:31 +0800

    refractor TranslationEvent using abstract class

--- Assets/__Scripts/Map/Events/BeatmapLightTranslationEvent.cs
+++ Assets/__Scripts/Map/Events/BeatmapLightTranslationEvent.cs
@@ -4,20 +4,14 @@ using System.Collections.Generic;
 using SimpleJSON;
 using UnityEngine;
 
-public class BeatmapLightTranslationEventData : BeatmapObject
+public class BeatmapLightTranslationEventData : BeatmapLightEventBoxDataBase
 {
-    public float AddedBeat { get => Time; set => Time = value; } // b, actually it shouldn't be Time, but for simply comparison just make it as Time
+    // public float AddedBeat { get => Time; set => Time = value; } // b, actually it shouldn't be Time, but for simply comparison just make it as Time
     public int UsePrevious; // p
     public int EaseType; // e
     public float TranslateValue; // t
 
-    public BeatmapLightTranslationEventData(JSONNode node)
-    {
-        AddedBeat = RetrieveRequiredNode(node, "b").AsFloat;
-        UsePrevious = RetrieveRequiredNode(node, "p").AsInt;
-        EaseType = RetrieveRequiredNode(node, "e").AsInt;
-        TranslateValue = RetrieveRequiredNode(node, "t").AsFloat;
-    }
+    public BeatmapLightTranslationEventData(JSONNode node) => LoadFromJson(node);
 
     public BeatmapLightTranslationEventData(float b, int p, int e, float t)
     {
@@ -27,98 +21,73 @@ public class BeatmapLightTranslationEventData : BeatmapObject
         TranslateValue = t;
     }
 
-    public BeatmapLightTranslationEventData()
+    public BeatmapLightTranslationEventData(): base()
     {
     }
 
-    public override ObjectType BeatmapType { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-
-    public override JSONNode ConvertToJson()
+    protected override void LoadFromJsonImpl(ref JSONNode node)
     {
-        JSONNode node = new JSONObject();
-        node["b"] = Math.Round(AddedBeat, DecimalPrecision);
+        UsePrevious = RetrieveRequiredNode(node, "p").AsInt;
+        EaseType = RetrieveRequiredNode(node, "e").AsInt;
+        TranslateValue = RetrieveRequiredNode(node, "t").AsFloat;
+    }
+    protected override void ConvertToJsonImpl(ref JSONNode node)
+    {
         node["p"] = UsePrevious;
         node["e"] = EaseType;
         node["t"] = TranslateValue;
-        return node;
     }
-    protected override bool IsConflictingWithObjectAtSameTime(BeatmapObject other, bool deletion = false) => throw new System.NotImplementedException();
 }
 
-public class BeatmapLightTranslationEventBox : BeatmapObject
+public class BeatmapLightTranslationEventBox : BeatmapLightEventBoxBase<BeatmapLightTranslationEventData>
 {
-    public BeatmapLightEventFilter Filter; // f
-    public float Distribution; // w
-    public int DistributionType; // d
+    // public BeatmapLightEventFilter Filter; // f
+    // public float Distribution; // w
+    // public int DistributionType; // d
     public float TranslationDistribution; // s
     public int TranslationDistributionType; // t
     public int Axis; // a
     public int Flip; // r
     public int TranslationAffectFirst; // b
-    public int DistributionEaseType; // i
-    public List<BeatmapLightTranslationEventData> EventDatas = new List<BeatmapLightTranslationEventData>(); // l
+    // public int DistributionEaseType; // i
+    // public List<BeatmapLightTranslationEventData> EventDatas = new List<BeatmapLightTranslationEventData>(); // l
 
-    public BeatmapLightTranslationEventBox(JSONNode node)
+    public BeatmapLightTranslationEventBox(JSONNode node) => LoadFromJson(node);
+
+    public BeatmapLightTranslationEventBox(): base()
     {
-        Filter = new BeatmapLightEventFilter(RetrieveRequiredNode(node, "f"));
-        Distribution = RetrieveRequiredNode(node, "w").AsFloat;
-        DistributionType = RetrieveRequiredNode(node, "d").AsInt;
+        TranslationDistributionType = 1;
+    }
+
+    public override string EventDataKey => "l";
+
+    protected override void LoadFromJsonImpl(ref JSONNode node)
+    {
         TranslationDistribution = RetrieveRequiredNode(node, "s").AsFloat;
         TranslationDistributionType = RetrieveRequiredNode(node, "t").AsInt;
         Axis = RetrieveRequiredNode(node, "a").AsInt;
         Flip = RetrieveRequiredNode(node, "r").AsInt;
         TranslationAffectFirst = RetrieveRequiredNode(node, "b").AsInt;
-        DistributionEaseType = RetrieveRequiredNode(node, "i").AsInt;
-        foreach (var n in RetrieveRequiredNode(node, "l"))
-        {
-            EventDatas.Add(new BeatmapLightTranslationEventData(n));
-        }
     }
-
-    public BeatmapLightTranslationEventBox()
+    protected override void ConvertToJsonImpl(ref JSONNode node)
     {
-        DistributionType = 1;
-        TranslationDistributionType = 1;
-    }
-
-    public override ObjectType BeatmapType { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
-
-    public override JSONNode ConvertToJson()
-    {
-        JSONNode node = new JSONObject();
-        node["f"] = Filter.ConvertToJson();
-        node["w"] = Distribution;
-        node["d"] = DistributionType;
         node["s"] = TranslationDistribution;
         node["t"] = TranslationDistributionType;
         node["a"] = Axis;
         node["r"] = Flip;
         node["b"] = TranslationAffectFirst;
-        node["i"] = DistributionEaseType;
-        var eventDatas = new JSONArray();
-        foreach (var ed in EventDatas) eventDatas.Add(ed.ConvertToJson());
-
-        node["l"] = eventDatas;
-        return node;
     }
-    protected override bool IsConflictingWithObjectAtSameTime(BeatmapObject other, bool deletion = false) => throw new System.NotImplementedException();
 }
 
 
-public class BeatmapLightTranslationEvent: BeatmapObject
+public class BeatmapLightTranslationEvent: BeatmapLightEventBase<BeatmapLightTranslationEventBox, BeatmapLightTranslationEventData>
 {
-    public float Beat { get => Time; set => Time = value; } // b
-    public int Group; // g
-    public List<BeatmapLightTranslationEventBox> EventBoxes = new List<BeatmapLightTranslationEventBox>(); // e
+    // public float Beat { get => Time; set => Time = value; } // b
+    // public int Group; // g
+    // public List<BeatmapLightTranslationEventBox> EventBoxes = new List<BeatmapLightTranslationEventBox>(); // e
 
-    public BeatmapLightTranslationEvent(JSONNode node)
+    public BeatmapLightTranslationEvent(JSONNode node): base(node)
     {
-        Beat = RetrieveRequiredNode(node, "b").AsFloat;
-        Group = RetrieveRequiredNode(node, "g").AsInt;
-        foreach (var n in RetrieveRequiredNode(node, "e"))
-        {
-            EventBoxes.Add(new BeatmapLightTranslationEventBox(n));
-        }
     }
 
     public BeatmapLightTranslationEvent(float b, int g, BeatmapLightTranslationEventBox e)
@@ -128,54 +97,9 @@ public class BeatmapLightTranslationEvent: BeatmapObject
         EventBoxes.Add(e);
     }
 
-    public BeatmapLightTranslationEvent()
+    public BeatmapLightTranslationEvent(): base()
     {
-        var filter = new BeatmapLightEventFilter();
-        var eb = new BeatmapLightTranslationEventBox();
-        var ebd = new BeatmapLightTranslationEventData();
-        eb.Filter = filter;
-        eb.EventDatas.Add(ebd);
-        EventBoxes.Add(eb);
-    }
-
-    public static List<BeatmapLightTranslationEvent> SplitEventBoxes(BeatmapLightTranslationEvent e)
-    {
-        var ret = new List<BeatmapLightTranslationEvent>();
-        foreach (var eb in e.EventBoxes)
-        {
-            ret.Add(new BeatmapLightTranslationEvent(e.Time, e.Group, eb));
-        }
-        return ret;
     }
 
     public override ObjectType BeatmapType { get; set; } = ObjectType.LightTranslationEvent;
-
-    public override JSONNode ConvertToJson()
-    {
-        var node = new JSONObject();
-        node["b"] = Math.Round(Beat, DecimalPrecision);
-        node["g"] = Group;
-        var eventBoxes = new JSONArray();
-        foreach (var eb in EventBoxes) eventBoxes.Add(eb.ConvertToJson());
-
-        node["e"] = eventBoxes;
-        return node;
-    }
-    protected override bool IsConflictingWithObjectAtSameTime(BeatmapObject other, bool deletion = false) { return false; }
-
-    public Vector2 GetPosition(PlatformDescriptorV3 platformDescriptor)
-    {
-        return new Vector2(platformDescriptor.GroupIdToLaneIndex(Group) + 0.5f, 0.5f);
-    }
-
-    public override void Apply(BeatmapObject originalData)
-    {
-        base.Apply(originalData);
-        if (originalData is BeatmapLightTranslationEvent obj)
-        {
-            Beat = obj.Beat;
-            Group = obj.Group;
-            EventBoxes = obj.EventBoxes;
-        }
-    }
 }
