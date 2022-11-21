9d1242a869b05fe5446531ae07466663e52a3c11
CCDV2 <denghaoyuan1100@126.com>
2022-11-22 02:00:49 +0800

    add declaration of translation event's container, collection and event

---
+++ Assets/__Scripts/Map/Events/BeatmapLightTranslationEventContainer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatmapLightTranslationEventContainer : BeatmapLightEventContainerBase<
+    BeatmapLightTranslationEvent,
+    BeatmapLightTranslationEventBox,
+    BeatmapLightTranslationEventData,
+    BeatmapLightTranslationEventContainer,
+    LightTranslationEventsContainer,
+    TranslationEvent
+    >
+{
+    
+}
---
+++ Assets/__Scripts/MapEditor/Grid/Collections/LightTranslationEventsContainer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTranslationEventsContainer : LightEventsContainerCollectionBase<
+    BeatmapLightTranslationEvent,
+    BeatmapLightTranslationEventBox,
+    BeatmapLightTranslationEventData,
+    BeatmapLightTranslationEventContainer,
+    LightTranslationEventsContainer,
+    TranslationEvent
+    >
+{
+
+}
