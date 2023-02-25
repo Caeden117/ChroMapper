113cbdb0f191fd56b20501a7d055b783259420ae
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2023-02-25 14:54:05 +1000

    Convert Chroma LightGradient generator to transition

--- Assets/__Scripts/MapEditor/Mapping/Strobe Generator/Passes/StrobeChromaPass.cs
+++ Assets/__Scripts/MapEditor/Mapping/Strobe Generator/Passes/StrobeChromaPass.cs
@@ -1,46 +1,45 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Beatmap.Base;
+using Beatmap.Enums;
 using Beatmap.Helper;
-using Beatmap.Shared;
-using SimpleJSON;
-using UnityEngine;
 
+// TODO rename this to StrobeTransitionPass
 public class StrobeChromaPass : StrobeGeneratorPass
 {
     private readonly string easing;
 
     public StrobeChromaPass(string easing) => this.easing = easing;
 
-    // TODO: Rework 2.0 gradients to use transition event type
-    public override bool IsEventValidForPass(BaseEvent @event) => !Settings.Instance.Load_MapV3 && @event.CustomColor != null && @event.CustomLightID is null;
+    public override bool IsEventValidForPass(BaseEvent @event) => @event.IsLightEvent();
 
     public override IEnumerable<BaseEvent> StrobePassForLane(IEnumerable<BaseEvent> original, int type,
         EventGridContainer.PropMode propMode, int[] propID)
     {
         var generatedObjects = new List<BaseEvent>();
 
+        generatedObjects.Add(BeatmapFactory.Clone(original.First()));
+
         var nonGradients = original.Where(x => x.CustomLightGradient == null);
-        for (var i = 0; i < nonGradients.Count() - 1; i++)
+        for (var i = 1; i < nonGradients.Count(); i++)
         {
-            var currentChroma = nonGradients.ElementAt(i);
-            var nextChroma = nonGradients.ElementAt(i + 1);
-
-            var generated = BeatmapFactory.Clone(currentChroma);
-            generated.CustomLightGradient = new ChromaLightGradient(
-                (Color)currentChroma.CustomColor, //Start color
-                (Color)nextChroma.CustomColor, //End color
-                nextChroma.Time - currentChroma.Time, //Duration
-                easing); //Duration
-
-            // Don't forget to replace our Chroma color with a Light Gradient in _customData
-            generated.CustomData.Add("_lightGradient", generated.CustomLightGradient.ToJson());
-            generated.CustomData.Remove("_color");
+            var generated = BeatmapFactory.Clone(nonGradients.ElementAt(i));
+            if (generated.IsBlue)
+            {
+                generated.Value = (int)LightValue.BlueTransition;
+            }
+            else if (generated.IsRed)
+            {
+                generated.Value = (int)LightValue.RedTransition;
+            }
+            else if (generated.IsWhite)
+            {
+                generated.Value = (int)LightValue.WhiteTransition;
+            }
+            generated.CustomEasing = easing;
 
             generatedObjects.Add(generated);
         }
-
-        generatedObjects.Add(BeatmapFactory.Clone(original.Last()));
 
         return generatedObjects;
     }
