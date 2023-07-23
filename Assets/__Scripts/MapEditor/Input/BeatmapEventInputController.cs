aa3a90feaf17d0e1572bcc9d65df42dd141dca14
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2023-07-23 12:57:06 +1000

    Update gradient display on event tweak

--- Assets/__Scripts/MapEditor/Input/BeatmapEventInputController.cs
+++ Assets/__Scripts/MapEditor/Input/BeatmapEventInputController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using Beatmap.Appearances;
 using Beatmap.Base;
 using Beatmap.Containers;
+using Beatmap.Enums;
 using Beatmap.Helper;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -89,6 +90,8 @@ public class BeatmapEventInputController : BeatmapInputController<EventContainer
             if (e.EventData.Value > 0 && e.EventData.Value <= 4) e.EventData.Value += 4; // blue to red
             else if (e.EventData.Value > 4 && e.EventData.Value <= 8) e.EventData.Value += 4; // red to white
             else if (e.EventData.Value > 8 && e.EventData.Value <= 12) e.EventData.Value -= 8; // white to blue
+
+            RefreshPrevEventContainer(e);
         }
 
         eventAppearanceSo.SetEventAppearance(e);
@@ -107,6 +110,8 @@ public class BeatmapEventInputController : BeatmapInputController<EventContainer
         {
             e.EventData.FloatValue += 0.1f * modifier;
             if (e.EventData.FloatValue < 0) e.EventData.FloatValue = 0;
+
+            RefreshPrevEventContainer(e);
         }
         else if (e.EventData.IsLaneRotationEvent())
         {
@@ -157,16 +162,23 @@ public class BeatmapEventInputController : BeatmapInputController<EventContainer
             e.EventData.Value += modifier;
 
             if (e.EventData.Value < 0) e.EventData.Value = 0;
+            if (e.EventData.Value > 12 && e.EventData.IsLightEvent()) e.EventData.Value = 12;
 
-            if (e.EventData.IsLightEvent())
-            {
-                if (e.EventData.Value > 12)
-                    e.EventData.Value = 12;
-            }
+            RefreshPrevEventContainer(e);
         }
 
         eventAppearanceSo.SetEventAppearance(e);
         BeatmapActionContainer.AddAction(new BeatmapObjectModifiedAction(e.ObjectData, e.ObjectData, original));
     }
 
+    private void RefreshPrevEventContainer(EventContainer e)
+    {
+        var prevEvent = e.EventData.Prev;
+        if (prevEvent != null)
+        {
+            var collection = BeatmapObjectContainerCollection.GetCollectionForType(ObjectType.Event);
+            if (collection.LoadedContainers.TryGetValue(prevEvent, out var container))
+                (container as EventContainer).RefreshAppearance();
+        }
+    }
 }
