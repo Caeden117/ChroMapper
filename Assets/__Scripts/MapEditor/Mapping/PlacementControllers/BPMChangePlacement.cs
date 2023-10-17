bd5760baa835a9beafcba39419ccc242e2f74825
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2023-10-17 21:30:40 +1000

    WIP - Beat reset option on bpm placement

--- Assets/__Scripts/MapEditor/Mapping/PlacementControllers/BPMChangePlacement.cs
+++ Assets/__Scripts/MapEditor/Mapping/PlacementControllers/BPMChangePlacement.cs
@@ -9,6 +9,35 @@ using UnityEngine;
 
 public class BPMChangePlacement : PlacementController<BaseBpmEvent, BpmEventContainer, BPMChangeGridContainer>
 {
+    private DialogBox createBpmEventDialogueBox;
+    private TextBoxComponent bpmTextInput;
+    private ToggleComponent resetBeatToggle;
+
+    internal override void Start()
+    {
+        base.Start();
+
+        // Create and cache dialog box for later use
+        createBpmEventDialogueBox = PersistentUI.Instance
+            .CreateNewDialogBox()
+            .DontDestroyOnClose();
+
+        // TODO: Localise these & and initialise with last bpm value
+        bpmTextInput = createBpmEventDialogueBox
+            .AddComponent<TextBoxComponent>()
+            .WithLabel("Bpm");
+
+        resetBeatToggle = createBpmEventDialogueBox
+            .AddComponent<ToggleComponent>()
+            .WithLabel("Reset Beat")
+            .WithInitialValue(false);
+
+        createBpmEventDialogueBox.OnQuickSubmit(() => AttemptPlaceBpmChange(bpmTextInput.Value, resetBeatToggle.Value));
+
+        createBpmEventDialogueBox.AddFooterButton(null, "PersistentUI", "cancel");
+        createBpmEventDialogueBox.AddFooterButton(() => AttemptPlaceBpmChange(bpmTextInput.Value, resetBeatToggle.Value), "PersistentUI", "ok");
+    }
+
     public override BeatmapAction GenerateAction(BaseObject spawned, IEnumerable<BaseObject> conflicting) =>
         new BeatmapObjectPlacementAction(spawned, conflicting, $"Placed a BPM Event at time {spawned.JsonTime}");
 
@@ -28,26 +57,50 @@ public class BPMChangePlacement : PlacementController<BaseBpmEvent, BpmEventCont
 
     internal override void ApplyToMap()
     {
-        var lastBpm = objectContainerCollection.FindLastBpm(SongBpmTime, false)?.Bpm ??
-                      BeatSaberSongContainer.Instance.Song.BeatsPerMinute;
-        PersistentUI.Instance.ShowInputBox("Mapper", "bpm.dialog", AttemptPlaceBpmChange,
-            "", lastBpm.ToString());
+        createBpmEventDialogueBox
+            .WithTitle("Mapper", "bpm.dialog")
+            .Open();
     }
 
-    private void AttemptPlaceBpmChange(string obj)
+    private void AttemptPlaceBpmChange(string obj, bool willResetGrid)
     {
         if (string.IsNullOrEmpty(obj) || string.IsNullOrWhiteSpace(obj)) return;
         if (float.TryParse(obj, out var bpm))
         {
-            queuedData.Bpm = bpm;
-            base.ApplyToMap();
+            if (willResetGrid && (Mathf.Abs(queuedData.JsonTime - Mathf.Round(queuedData.JsonTime)) > BeatmapObjectContainerCollection.Epsilon))
+            {
+                var prevBpm = objectContainerCollection.FindLastBpm(SongBpmTime, false)?.Bpm ??
+                          BeatSaberSongContainer.Instance.Song.BeatsPerMinute;
+                var oldTime = queuedData.JsonTime;
+                var jsonTimeOffset = 1 - oldTime % 1;
+
+                // Place a very fast bpm event slighty behind the original event to account for drift
+                var aVeryLargeBpm = 100000f;
+                var offsetRequiredInBeats = jsonTimeOffset * prevBpm / (aVeryLargeBpm - prevBpm);
+                var offsetEvent = BeatmapFactory.BpmEvent(oldTime - offsetRequiredInBeats, aVeryLargeBpm);
+                objectContainerCollection.SpawnObject(offsetEvent, out var fatConflicting);
+
+                // Place the bpm event on the next beat
+                var queuedEvent = BeatmapFactory.BpmEvent(Mathf.Ceil(oldTime), bpm);
+                objectContainerCollection.SpawnObject(queuedEvent, out var queuedConflicting);
+
+                BeatmapActionContainer.AddAction(new ActionCollectionAction(new List<BeatmapAction>{
+                    GenerateAction(offsetEvent, fatConflicting),
+                    GenerateAction(queuedEvent, queuedConflicting)
+                }));
+            }
+            else
+            {
+                queuedData.Bpm = bpm;
+                base.ApplyToMap();
+            }
         }
         else
         {
-            var lastBpm = objectContainerCollection.FindLastBpm(SongBpmTime, false)?.Bpm ??
-                          BeatSaberSongContainer.Instance.Song.BeatsPerMinute;
-            PersistentUI.Instance.ShowInputBox("Mapper", "bpm.dialog.invalid",
-                AttemptPlaceBpmChange, "", lastBpm.ToString());
+            // TODO: This path doesn't work
+            createBpmEventDialogueBox
+                .WithTitle("Mapper", "bpm.dialog.invalid")
+                .Open();
         }
     }
 }
