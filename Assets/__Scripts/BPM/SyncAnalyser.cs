9b62eb97b43a7ddc13ffe74fb4a01191f56cc6ec
Caeden117 <caeden.s@outlook.com>
2023-11-08 12:48:20 -0800

    Add "indeterminant" box if results > threshold

--- Assets/__Scripts/BPM/SyncAnalyser.cs
+++ Assets/__Scripts/BPM/SyncAnalyser.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 
 public class SyncAnalyser : MonoBehaviour
 {
+    // How many results before CM considers bpm detection "indeterminant"
+    private const int indeterminantBPMThreshold = 5;
+
     private readonly AudioSync.SyncAnalyser syncAnalysis = new AudioSync.SyncAnalyser(89, 205);
 
     private IList<SyncResult> results;
@@ -14,6 +17,7 @@ public class SyncAnalyser : MonoBehaviour
 
     private DialogBox detectingBPMBox;
     private DialogBox bpmResultsBox;
+    private DialogBox bpmIndeterminantBox;
     private DropdownComponent bpmResultsDropdown;
 
     public void Analyse()
@@ -45,15 +49,25 @@ public class SyncAnalyser : MonoBehaviour
         // so we just detect when the thread dies.
         if (detectionThread != null && !detectionThread.IsAlive)
         {
-            OnComplete();
+            detectingBPMBox.Close();
+
+            // Multiple results may imply troubles with BPM detection, so should be taken with caution.
+            // This, show an intermediate dialog box before showing the results.
+            if (results.Count > indeterminantBPMThreshold)
+            {
+                bpmIndeterminantBox.Open();
+            }
+            else
+            {
+                OnComplete();
+            }
+
             detectionThread = null;
         }
     }
 
     private void OnComplete()
     {
-        detectingBPMBox.Close();
-
         var items = results.Select(it => $"{it.BPM:N2} BPM | Offset: {it.Offset:N2} sec");
         bpmResultsDropdown.WithOptions(items);
 
@@ -86,7 +100,18 @@ public class SyncAnalyser : MonoBehaviour
         bpmResultsDropdown = bpmResultsBox.AddComponent<DropdownComponent>()
             .WithLabel("BPM");
 
-        bpmResultsBox.AddFooterButton(ApplyBPM, "PersistentUI", "submit");
         bpmResultsBox.AddFooterButton(null, "PersistentUI", "cancel");
+        bpmResultsBox.AddFooterButton(ApplyBPM, "PersistentUI", "submit");
+
+        // BPM Indeterminant
+        bpmIndeterminantBox = PersistentUI.Instance.CreateNewDialogBox()
+            .WithTitle("BPM Detection Results")
+            .DontDestroyOnClose();
+
+        bpmIndeterminantBox.AddComponent<TextComponent>()
+            .WithInitialValue("ChroMapper has detected several tempos for this song. BPM detection may have been indeterminant.\n\nThis is usually a result of multiple BPM changes, a drifting BPM, or some other difficulty with the song.\n\nWould you like to see these results?");
+
+        bpmIndeterminantBox.AddFooterButton(null, "PersistentUI", "no");
+        bpmIndeterminantBox.AddFooterButton(OnComplete, "PersistentUI", "yes");
     }
 }
