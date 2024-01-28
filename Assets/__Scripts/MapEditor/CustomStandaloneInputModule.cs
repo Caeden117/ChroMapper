2946950094a97b975a3f920eb0ab77d5e6292e06
Caeden Statia <caeden.s@outlook.com>
2024-01-27 17:04:06 -0800

    Switch remaining event systems to new Input System

--- Assets/__Scripts/MapEditor/CustomStandaloneInputModule.cs
+++ Assets/__Scripts/MapEditor/CustomStandaloneInputModule.cs
@@ -1,35 +1,23 @@
 ﻿using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
 
 /*  /class CustomStandaloneInputModule
 *  /brief A StandaloneInputModule with access to buttons' PointerEventData and RaycasterModule checking
 */
-public class CustomStandaloneInputModule : StandaloneInputModule
+public class CustomStandaloneInputModule : InputSystemUIInputModule
 {
-    /// <summary>
-    ///     Returns current PointerEventData
-    /// </summary>
-    public PointerEventData GetPointerData(int pointerId = kMouseLeftId)
-    {
-        m_PointerData.TryGetValue(pointerId, out var pointerData);
-        return pointerData ?? new PointerEventData(EventSystem.current);
-    }
-
     /// <summary>
     ///     Returns true if current raycast has hit an game object using Raycaster T
     /// </summary>
-    public bool IsPointerOverGameObject<T>(int pointerId = kMouseLeftId, bool includeDerived = false)
+    public bool IsPointerOverGameObject<T>(int pointerId, bool includeDerived = false)
         where T : BaseRaycaster
     {
-        if (IsPointerOverGameObject(pointerId))
-        {
-            if (m_PointerData.TryGetValue(pointerId, out var pointerEventData))
-            {
-                return includeDerived
-                    ? pointerEventData.pointerCurrentRaycast.module is T
-                    : pointerEventData.pointerCurrentRaycast.module.GetType() == typeof(T);
-            }
-        }
+        if (!IsPointerOverGameObject(pointerId)) return false;
 
-        return false;
+        var raycastResult = GetLastRaycastResult(pointerId);
+
+        return includeDerived
+            ? raycastResult.module is T
+            : raycastResult.module.GetType() == typeof(T);
     }
 }
