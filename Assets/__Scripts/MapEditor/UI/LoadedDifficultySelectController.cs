72725e3974225d4ab2ab7950e2dc8a503caca425
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2025-01-02 18:14:55 +1000

    Add switching difficulty functionality to editor

---
+++ Assets/__Scripts/MapEditor/UI/LoadedDifficultySelectController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Beatmap.Info;
+using TMPro;
+using UnityEngine;
+
+public class LoadedDifficultySelectController : MonoBehaviour
+{
+    [SerializeField] private MapLoader mapLoader;
+    [SerializeField] private TMP_Dropdown dropdown;
+    
+    private List<InfoDifficulty> setDifficulties;
+
+    private void Start()
+    {
+        // ToList() to keep the ordering the same in case the ordering changes on save (which it currently does)
+        setDifficulties = BeatSaberSongContainer.Instance.MapDifficultyInfo.ParentSet.Difficulties.ToList();
+        
+        var options = setDifficulties.Select(x => new TMP_Dropdown.OptionData(x.Difficulty)); 
+        
+        dropdown.options = new List<TMP_Dropdown.OptionData>(options);
+        dropdown.value = setDifficulties.IndexOf(BeatSaberSongContainer.Instance.MapDifficultyInfo);
+        
+        if (setDifficulties.Count == 1)
+        {
+            // No other diffs to switch so disable the dropdown
+            dropdown.interactable = false;
+        }
+        else
+        {
+            dropdown.onValueChanged.AddListener(OnDropdownChange);
+        }
+    }
+
+    private void OnDropdownChange(int value)
+    {
+        BeatSaberSongContainer.Instance.MapDifficultyInfo = setDifficulties[value];
+        
+        var newMap = BeatSaberSongUtils.GetMapFromInfoFiles(
+            BeatSaberSongContainer.Instance.Info,
+            BeatSaberSongContainer.Instance.MapDifficultyInfo);
+        mapLoader.UpdateMapData(newMap);
+        mapLoader.HardRefresh();
+        
+        BeatSaberSongContainer.Instance.Map = newMap;
+    }
+}
+
