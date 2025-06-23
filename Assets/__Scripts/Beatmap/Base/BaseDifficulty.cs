Caeden117/ChroMapper

commit 159f492fa495421d4cb34200242bd4068e2b227a

Fix official editor warning about out of sync BPMInfo

Assets/__Scripts/Beatmap/Base/BaseDifficulty.cs
@@ -412,9 +412,9 @@ namespace Beatmap.Base
             }
 
             // Write Bpm file
-            var bpmRegions = BaseBpmInfo.GetBpmInfoRegions(BpmEvents, songContainer.Info.BeatsPerMinute,
-                songContainer.LoadedSongSamples, songContainer.LoadedSongFrequency);
-            var bpmInfo = new BaseBpmInfo { BpmRegions = bpmRegions }.InitWithSongContainerInstance();
+            var bpmInfo = new BaseBpmInfo().InitWithSongContainerInstance();
+            bpmInfo.BpmRegions = BaseBpmInfo.GetBpmInfoRegions(BpmEvents, songContainer.Info.BeatsPerMinute,
+                bpmInfo.AudioSamples, bpmInfo.AudioFrequency);
 
             // Don't write if created difficulty before supplying audio file
             if (bpmInfo.AudioSamples != default)

Assets/__Scripts/Beatmap/Info/Base/BaseBpmInfo.cs
@@ -15,7 +15,9 @@ namespace Beatmap.Info
         public BaseBpmInfo InitWithSongContainerInstance()
         {
             Version = BeatSaberSongContainer.Instance.Info.Version[0] == '4' ? "4.0.0" : "2.0.0";
-            AudioSamples = BeatSaberSongContainer.Instance.LoadedSongSamples;
+            
+            // This -1 is so the official editor doesn't yell at you that the audio is out of sync
+            AudioSamples = BeatSaberSongContainer.Instance.LoadedSongSamples - 1; 
             AudioFrequency = BeatSaberSongContainer.Instance.LoadedSongFrequency;
 
             return this;

commit 51bb70e29db73266a3f29b44fb998d7f6fdc4769

Fix AudioData being created when no audio is supplied
Regression from commit 159f492fa495421d4cb34200242bd4068e2b227a.

Assets/__Scripts/Beatmap/Base/BaseDifficulty.cs
@@ -417,7 +417,7 @@ namespace Beatmap.Base
                 bpmInfo.AudioSamples, bpmInfo.AudioFrequency);
 
             // Don't write if created difficulty before supplying audio file
-            if (bpmInfo.AudioSamples != default)
+            if (bpmInfo.AudioSamples > 0)
             {
                 var bpmOutputJson = songContainer.Info.MajorVersion switch
                 {