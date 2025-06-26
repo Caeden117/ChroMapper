47622cdded49181a5281cf93334724186190bad5
Bullet <68104413+XAce1337manX@users.noreply.github.com>
2025-06-27 00:04:23 +1000

    Clean up unused code

--- Assets/__Scripts/UI/SongEditMenu/CustomDifficultyPropertiesController.cs
+++ Assets/__Scripts/UI/SongEditMenu/CustomDifficultyPropertiesController.cs
@@ -2,15 +2,7 @@
 
 public class CustomDifficultyPropertiesController : MonoBehaviour
 {
-    // TODO: Add other custom properties
-    //  * ForceOneSaber
-    //  * ShowRotationNoteSpawnLines
-    //  * Warnings
-    //  * Information
-    
     [SerializeField] private GameObject EditDialog;
-    [SerializeField] private EnvRemoval EnvRemoval;
     
     public void ToggleEditDialog() => EditDialog.SetActive(!EditDialog.activeSelf);
 }
-
